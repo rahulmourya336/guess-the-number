a051032824096ab25364716ee1203609e3a5a2d0
rahulmourya336@gmail.com <rahul@yahoo.com>
2019-02-02 19:20:51 +0530

    Add streamline input

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
+/**
+ * [TODO]
+ *   1. Render text-box on level select.
+ *   2. Write test cases for application.
+ *   3. Make global scoreboard of game.
+ * 
+ */
+
 window.onload = _ => {
     createConfetti(0, true);
     clearGeneratedNumber();
     console.log('View Loaded');
     // generateTargetNumber(10);
-    checkView();
+    // checkView();
 };
 
+const elementRender = (parentElement, childElement, classList, elementId) => {
+
+}
+
+const generateInputView = _ => {
+    let parent = document.getElementById('input__guessList');
+    let __maindiv = document.createElement('div');
+    __maindiv.className = 'empty-action empty-width';
+    __maindiv.id = 'actionInterface';
+
+    let __inputDiv = document.createElement('div');
+    __inputDiv.className = 'input-group';
+
+    let __input = document.createElement('input');
+    __input.type = 'number';
+    __input.className = "form-input";
+    __input.placeholder = '44';
+    __input.id = 'inputValue';
+
+    let __button = document.createElement('button');
+    __button.className = 'btn btn-primary input-group-btn';
+    __button.setAttribute('onclick', 'startGame()');
+    __button.innerText = 'Wild Guess';
+
+    let __helperMessage = document.createElement('p');
+    __helperMessage.className = 'empty-subtitle small mt-2 mb-2';
+    __helperMessage.id = 'status_hint';
+    __helperMessage.innerText = 'Enter a number to start guessing';
+
+    let __guessList = document.createElement('div');
+    __guessList.id = 'guessList';
+    __guessList.className = 'mt-2 mark';
+
+    __inputDiv.append(__input);
+    __inputDiv.append(__button);
+
+    __maindiv.append(__inputDiv);
+    __maindiv.append(__helperMessage);
+
+    parent.append(__maindiv);
+    parent.append(__guessList);
+
+}
+
 const checkView = _ => {
-    if (localStorage.length === 0) {
-        $('#actionInterface').hide();
-        $('#guessList').hide();
-    } else {
+    if (localStorage.length > 0) {
         $('#levelSelect').hide();
-        $('#actionInterface').show();
-        $('#guessList').show();
     }
 }
 
@@ -36,6 +83,7 @@ const generateTargetNumber = (number) => {
     let generatedNumber = Math.floor(Math.random() * number);
     localStorage.setItem('NUMBER', generatedNumber);
     checkView();
+    generateInputView();
 }
 
 const makeUserGuessList = (number) => {
